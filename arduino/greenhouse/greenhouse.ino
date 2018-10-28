147a320af530e6e911c308571e9bc350a731588d
biloniis <biloniis@yahoo.com>
2018-10-28 16:38:44 +0200

    relise

--- arduino/greenhouse/greenhouse.ino
+++ arduino/greenhouse/greenhouse.ino
@@ -4,29 +4,30 @@
 #define DHTPIN 2     // what digital pin we're connected to
 
 #define DHTTYPE DHT11   // DHT 11
-//iarduino_RTC time(RTC_DS1302, 1, 2, 3);
+#define HEATER_PIN 7 //pin for heater reley module
+#define LIGHT_PIN 3 //pin for light relay module
+
 iarduino_RTC time(RTC_DS1307);
 DHT dht(DHTPIN, DHTTYPE);
 //Application constants and variables
-#define MIN_TEMP 14 // temperature for switch on heater 
-#define MAX_TEMP 18 // temperature for switch off heater
-#define HEATER_PIN 3 //pin for heater reley module
-#define LIGHT_PIN 7 //pin for light relay module
-boolean isHeaterOn;
-boolean isLightOn;
+const float MIN_TEMP=14.0; // temperature for switch on heater 
+const float MAX_TEMP=18.0; // temperature for switch off heater
+
+bool isHeaterOn;
+bool isLightOn;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("DHTxx test!");
 	pinMode(HEATER_PIN, OUTPUT);
-	digitalWrite(HEATER_PIN, LOW);
+	digitalWrite(HEATER_PIN, HIGH);
 	isHeaterOn = false;
 	pinMode(LIGHT_PIN, OUTPUT);
-	digitalWrite(LIGHT_PIN, LOW);
-	isLightOn = false;
+	digitalWrite(LIGHT_PIN, HIGH);
+	isLightOn = true;
 	time.begin();
   dht.begin();
-	//time.settime(0,23,7,22,10,18,1);
+	time.settime(0,16,16,28,10,18,7);
 }
 
 void loop() {
@@ -45,38 +46,28 @@ void loop() {
 	{
 		Serial.println("Heater on!");
 		isHeaterOn = true;
-		digitalWrite(HEATER_PIN, HIGH);
+		digitalWrite(HEATER_PIN, LOW);
 	}
 	if(t > MAX_TEMP && isHeaterOn)
 	{
 		Serial.println("Heater off!");
 		isHeaterOn = false;	
-		digitalWrite(HEATER_PIN, LOW);
+		digitalWrite(HEATER_PIN, HIGH);
 	}
-	int hours = time.gettime("H");
 	Serial.println(time.gettime("d-m-Y, H:i:s, D"));
-	if( hours > 17 && hours <22 && !isLightOn )
+  time.gettime();
+  int hours = time.Hours;
+  Serial.println(hours);
+	if( (hours >= 17 && hours < 22 && !isLightOn) ||  ( hours > 5 && hours <= 8 && !isLightOn ))
 	{
 		Serial.println("Light on!");
 		isLightOn = true;
-		digitalWrite(LIGHT_PIN, HIGH);
+		digitalWrite(LIGHT_PIN, LOW);
 	}
-	if( hours > 5 && hours < 8 && !isLightOn )
-	{
-		Serial.println("Light on!");
-		isLightOn = true;
-		digitalWrite(LIGHT_PIN, HIGH);
-	}
-	if( hours > 22 && isLightOn )
+	if( (hours >= 22 ||(hours > 8 && hours < 17)) && isLightOn )
 	{
 		Serial.println("Light off!");
 		isLightOn = false;
-		digitalWrite(LIGHT_PIN, LOW);
-	}
-	if( hours > 8 && hours < 17 && isLightOn )
-	{
-		Serial.println("Light off!");
-		isLightOn = false;
-		digitalWrite(LIGHT_PIN, LOW);
+		digitalWrite(LIGHT_PIN, HIGH);
 	}
 }
